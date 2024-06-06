feda89a267547e601aadd557d48ffb68269d03d6
Mashkus <Mashkus@users.noreply.github.com>
2024-06-06 22:53:03 +0200

    added numerical_sort, choosing between sorts, changes to report_info to allow both types of sorts.

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def get_word_count(text: str) -> int:
     """returns length of a text.split()"""
     return len(text.split())
 
-def alphabetical_sort(text: str) -> dict[str:int]:
+def get_character_dict(text: str) -> dict[str:int]:
     """
     takes text argument, returning a alphabetically sorted 
     dictionary of how many of each alphabetical character it contains
@@ -16,27 +16,57 @@ def alphabetical_sort(text: str) -> dict[str:int]:
     character_dict = {}
     text = text.lower()
     for character in text:
-        if character.isalpha():
-            character_dict.update({character:(character_dict.get(character, 0)+1)})
-    alpha_sort = dict(sorted(character_dict.items()))
-    return alpha_sort
+        if not character.isalpha():
+            continue
+        character_dict.update({character:(character_dict.get(character, 0)+1)})
+    return character_dict
 
-# def numerical_sort(text: str) -> list[dict[str:int]]:
-#     text = text.lower()
-#     for character in text:
-        
-#     alpha_sort = dict(sorted(character_dict.items()))
-#     return alpha_sort
+def choose_sort():
+    """Give the choice of numerical or alphabetical sort"""
+    chosen = False
+    choice = input("Do you wish to sort alphabetically, or numerically? A/N ")
+    while not chosen:
+        if choice.lower() == "a":
+            choice = "a"
+            chosen = True
+            return choice
 
-def report_info(path, dictionary: dict[str:int]):
+        if choice.lower() == "n":
+            choice = "n"
+            chosen = True
+            return choice
+
+        print("Invalid input, choose between 'A' (alphabetical) or 'N' (numerical)")
+
+
+def numerical_sort(characters: dict[str:int]) -> list[dict[str:int]]:
+    """returns sorted get_character_dict on its values"""
+    def sort_on(d):
+        return d["count"]
+    sorting_list = []
+    for key in characters:
+        sorting_list.append({"char":key,"count":characters[key]})
+    sorting_list.sort(reverse=True, key=sort_on)
+    return sorting_list
+
+
+def alphabetical_sort(characters: dict[str:int]) -> dict[str:int]:
+    """returns sorted get_character_dict on its keys"""
+    return dict(sorted(characters.items()))
+
+
+def report_info(path, collection: dict[str:int] | list[dict[str:int]]):
     """
     takes alphabetically ordered dict from get_character_amount()
     and prints out how many times each character shows up
     """
     print(f"--- Begin report of {path} --- \n")
-    sorted_dict = dictionary
-    for key in sorted_dict:
-        print(f"'{key.upper()}' shows up {sorted_dict[key]} times")
+    if isinstance(collection, dict):
+        for key in collection:
+            print(f"'{key.upper()}' shows up {collection[key]} times")
+    elif isinstance(collection, list):
+        for dictionary in collection:
+            print(f"'{dictionary['char'].upper()}' shows up {dictionary['count']} times")
     print("\n--- End Report ---")
 
 
@@ -44,9 +74,14 @@ def main():
     """main body for bookbot"""
     path = "books/frankenstein.txt"
     text = get_text(path)
-    
-    characters = alphabetical_sort(text)
-    report_info(path, characters)
+    characters = get_character_dict(text)
+    choice = choose_sort()
+    if choice == "n":
+        num_sorted = numerical_sort(characters)
+        report_info(path, num_sorted)
+    if choice == "a":
+        alpha_sorted = alphabetical_sort(characters)
+        report_info(path, alpha_sorted)
 
 
 if __name__ == "__main__":
